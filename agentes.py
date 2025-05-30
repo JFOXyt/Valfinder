b4a5b2d202c6650f83a2e0dfd03582fb7d8b1318
JD <113248132+JFOXyt@users.noreply.github.com>
2025-05-30 21:17:11 +0100

    Add files via upload

---
+++ agentes.py
@@ -0,0 +1,171 @@
+import tkinter
+from tkinter import END
+from tkinter import messagebox
+import tkinter.messagebox
+import requests
+from PIL import Image, ImageTk
+
+def butaoagentes(root,img,result_labels,gray):
+
+    for label in result_labels:
+        label.destroy()
+
+    result_labels.clear()
+
+    entry=tkinter.Entry(root
+                        ,font=('Arial',32))
+    
+    entry.place(x=25,y=325,height=60,width=550)
+
+    image=tkinter.PhotoImage(file=img[4]).subsample(6,6)
+    imagesearch=image
+
+    butaosearch=tkinter.Button(root,image=imagesearch
+                               ,cursor='hand2'
+                               ,border=0
+                               ,command=lambda:[butaosearch()]
+                               ,bg=gray)
+    
+    imagesearch.image=image
+    
+    butaosearch.place(x=600,y=320)
+
+    url=requests.get('https://valorant-api.com/v1/agents')
+    response=url.json()
+
+    def butaosearch():
+
+        for label in result_labels:
+            label.destroy()
+        result_labels.clear()
+
+        entryget=entry.get().lower()
+        entry.delete(0,END)
+
+        if url.status_code==200 and entryget!='':
+            lenagent=len(response['data'])
+
+            for i in range(lenagent):
+                pegarnomeagente=response['data'][i]['displayName'].lower()
+                isplayable=response['data'][i]['isPlayableCharacter']
+
+                if entryget==pegarnomeagente and isplayable:
+                    nomeagente=entryget
+                    description=response['data'][i]['description']
+
+                    imageagent=requests.get(response['data'][i]['fullPortrait'])
+
+                    with open('agentimage.png','wb') as ai:
+                        ai.write(imageagent.content)
+                    
+                    role=response['data'][i]['role']['displayName']
+                    roledescription=response['data'][i]['role']['description']
+                    roleagent=requests.get(response['data'][i]['role']['displayIcon'])
+
+                    with open('agentrole.png','wb') as ar:
+                        ar.write(roleagent.content)
+
+                    abilitylen=len(response['data'][i]['abilities'])
+
+                    for a in range(abilitylen):
+                        abilityicon=requests.get(response['data'][i]['abilities'][a]['displayIcon'])
+                        abilityname=response['data'][i]['abilities'][a]['displayName']
+                        abilitydescription=response['data'][i]['abilities'][a]['description']
+
+                        with open('iconability.png','wb') as ib:
+                            ib.write(abilityicon.content)
+
+                        abilitynamet=tkinter.Label(root,text=abilityname
+                                                    ,bg=gray
+                                                    ,font=('Arial',24,'bold'))
+                        
+                        abilitynamet.place(x=1000,y=430+150*a)
+                        result_labels.append(abilitynamet)
+
+                        abilityicondimension=Image.open(img[5])
+                        dimensao=abilityicondimension.width
+
+                        if dimensao==1024:
+                            dimensoes=13
+                        elif dimensao==128:
+                            dimensoes=2
+                        elif dimensao==512:
+                            dimensoes=13
+                        else:
+                            print('oh no',dimensao)
+
+                        abilityiconp=tkinter.PhotoImage(file=img[6]).subsample(dimensoes,dimensoes)
+
+                        abilityiconpt=tkinter.Label(root,image=abilityiconp
+                                                    ,bg=gray)
+                        
+                        abilityiconpt.abilityiconp=abilityiconp
+                        abilityiconpt.place(x=900,y=430+150*a)
+                        result_labels.append(abilityiconpt)
+
+                        abilitydescriptiont=tkinter.Label(root,text=abilitydescription
+                                                            ,wraplength=850
+                                                            ,bg=gray
+                                                            ,font=('Arial',14))
+                        
+                        abilitydescriptiont.place(x=1000,y=480+150*a)
+                        result_labels.append(abilitydescriptiont)
+                    
+                    agentimagep=Image.open(img[0])
+                    agentimagep=agentimagep.resize((agentimagep.width//3,agentimagep.height//3))
+                    agentimage=ImageTk.PhotoImage(agentimagep)
+
+                    agentimaget=tkinter.Label(root,image=agentimage,bg=gray)
+                    agentimaget.agentimage=agentimage
+                    agentimaget.place(x=-160,y=395)
+                    result_labels.append(agentimaget)
+
+
+                    nomeagentet=tkinter.Label(root,text=nomeagente
+                                              ,font=('Arial',44,'bold')
+                                              ,bg=gray)
+                    
+                    nomeagentet.place(x=325,y=396)
+                    result_labels.append(nomeagentet)
+
+                    descriptiont=tkinter.Label(root,text=description
+                                               ,font=('Arial',18)
+                                               ,bg=gray
+                                               ,wraplength=370)
+                    
+                    descriptiont.place(x=375,y=500)
+                    result_labels.append(descriptiont)
+
+                    agentrole=tkinter.PhotoImage(file=img[1]).subsample(2,2)
+                    agentrolet=tkinter.Label(root,image=agentrole
+                                             ,bg=gray)
+                    
+                    agentrolet.agentrole=agentrole
+                    agentrolet.place(x=325,y=750)
+                    result_labels.append(agentrolet)
+
+                    rolet=tkinter.Label(root,text=role
+                                        ,bg=gray
+                                        ,font=('Arial',32,'bold'))
+                    
+                    rolet.place(x=400,y=765)
+                    result_labels.append(rolet)
+
+                    roledescriptiont=tkinter.Label(root,text=roledescription
+                                                   ,bg=gray
+                                                   ,wraplength=370
+                                                   ,font=('Arial',18))
+                    
+                    roledescriptiont.place(x=350,y=855)
+                    result_labels.append(roledescriptiont)
+
+                    abilitiest=tkinter.Label(root,text='Abilities'
+                                             ,bg=gray
+                                             ,font=('Arial',44,'bold'))
+                    
+                    abilitiest.place(x=950,y=300)
+                    result_labels.append(abilitiest)
+    
+
+        else:
+            tkinter.messagebox.showerror('Error',"Check if you are conected to the wifi or if you entered an agent name")
