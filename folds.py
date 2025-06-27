3ba3dacde122da87ff27ca1216024aba173683bd
JD <113248132+JFOXyt@users.noreply.github.com>
2025-06-27 16:59:36 +0100

    gg

--- folds.py
+++
@@ -1,8 +0,0 @@
-import folium
-
-# Create a Map instance
-m = folium.Map(location=[80.25, 64.8],
-                   zoom_start=10, control_scale=True)
-
-outfp="C:\\Users\\Lenovo\\Desktop\\Valfinder\\base_map.html"
-m.save(outfp)
